032bbbb9b6c75cc755b6d3291cf07384035a4329
nick culpin <njculpin@gmail.com>
2016-03-29 07:57:25 -0700

    original geo position

--- revolve_from_origin.py
+++ revolve_from_origin.py
@@ -1,13 +1,19 @@
 # revolve from origin
 import maya.cmds as cmds
 
+# number of copies plus the angle of rotation
 iterations = 16
 angle = 22
+
+# original geometry position
+posX = 2
+posY = 2
+posZ = 2
 
 def freezeit():
     cmds.select(cmds.listRelatives(cmds.ls(geometry=True), p=True, path=True), r=True)
     cmds.makeIdentity(apply=True, t=1, r=1, s=1, n=0)
-    cmds.move(2, 2, 2, ".scalePivot",".rotatePivot", absolute=True)
+    cmds.move(posX, posY, posZ, ".scalePivot",".rotatePivot", absolute=True)
     revolve(iterations)
 
 def revolve(iterations):
@@ -17,7 +23,7 @@ def revolve(iterations):
         cmds.select()
         cmds.duplicate()
         cmds.makeIdentity(apply=True, t=1, r=1, s=1, n=0)
-        cmds.move(2, 2, 2, ".scalePivot",".rotatePivot", absolute=True)
+        cmds.move(posX, posY, posZ, ".scalePivot",".rotatePivot", absolute=True)
         cmds.rotate(0,angle,0)
         print ("iteration = ",str(iterations))
         revolve(iterations-1)
